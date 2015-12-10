3f0f21ac54b3c9453c54495103e272db0b84efd4
adamzjw <adamzjw@msn.com>
2015-12-10 18:22:43 -0500

    init use-case code

---
+++ example.py
@@ -0,0 +1,19 @@
+__author__ = 'adamzjw'
+
+from oath import Client
+from DataFetecher import DataFetcher
+from heartrateData import HeartrateTimeSeries
+
+clientID = ''
+clientSecret = ''
+client = Client(clientID, clientSecret)
+client.obtainConsent()
+client.requestToken()
+dataFetecher = DataFetcher(client.get_token())
+
+date = '2015-12-09'
+detail_level = '1sec'
+rawData = dataFetecher.fetch_heartrate_intraday(date, detail_level)
+
+heartrateTimeSeries = HeartrateTimeSeries(rawData)
+heartrateTimeSeries.generate_plot()
